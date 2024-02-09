e68f24db8425ace87a0370447f11b3e0dd3cbf70
PRIYA2508 <56288128+PRIYA2508@users.noreply.github.com>
2024-02-09 08:02:16 +0000

    new

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -2,4 +2,6 @@ const inteGer = 123
 let email_Id = "ps3869253"
 var passWord = "priya "
 console.table([inteGer,email_Id , passWord])
+passWord = "tillu"
+console.log(passWord);
 console.log("priya sharma")
