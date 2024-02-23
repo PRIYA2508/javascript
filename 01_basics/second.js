0e020982eb5f19f95d04dad2c286aa15bf7eefe6
Priya <ps3869253@gmail.com>
2024-02-23 23:21:22 +0530

    learn more basics

---
+++ 01_basics/second.js
@@ -0,0 +1,23 @@
+// ******************** Array **********************************
+// In javascript in Array we can store multiple items(means like number , string, bollean , char ..etc )uder a single virable name.
+
+const myArr   =[1,2,34,5,6]
+console.log(myArr[1])
+
+// syntax :- const vairable = new Array(items jo bhi store karna hai )
+const myarr =  new Array(1,2,3,4,5,6,7,8,9)
+console.log(myarr[9])
+// for checking the lenght of the array 
+//here in javascript there is a change  like we can change array length 
+const length = myarr.length;
+console.log(length);
+
+//opertaions in array 
+myarr.push(1)
+myarr.pop()
+console.log(myarr)
+console.log(myarr.reverse())
+ console.log()
+
+
+ 
